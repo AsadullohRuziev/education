ce841b2f441e0cdc1f479020efd55a7b6938ec77
Reactjs <asadullohruziev@gmail.com>
2022-02-04 08:15:38 +0500

    navbar finish

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+
+
+const App = () => (
+    <div className='app'>
+      <Navbar/>
+      <Footer/>
+    </div>
+);
+
+export default App;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css'
+
+
+const Footer = () => {
+  return(
+    <>
+        <div className='x'>hello</div>
+        <div className='h'>hello</div>
+    </>
+  )
+};
+
+export default Footer;
