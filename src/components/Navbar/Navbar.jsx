1acf346080c1e91dadea5b7e3bba3705d3e9e06a
Reactjs <asadullohruziev@gmail.com>
2022-02-07 18:09:07 +0500

    header  master

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import './style';
+import { Container, Logo, Link } from './style';
+
+
+
+const Navbar = () => {
+  return(
+    <Container>
+      <Router>
+        
+      </Router>
+    </Container>
+  )
+};
+
+
+export default Navbar;
---
+++ src/components/Navbar/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+
+
+export const Container = styled.div`
+    width: 100%;
+    display: flex;
+    background-color: red;
+`
+
+
+
+export const Logo = styled.div`
+    display: flex;
+
+`
+
+
+export const Link = styled(NavLink)`
+    display: flex;
+    font-size: 50px;
+`
