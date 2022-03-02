7569fd0b8ebfef30d8c3a544236fcf06ea44c24a
Intizom <asadullohruziev@gmail.com>
2022-03-02 12:59:55 +0500

    finish✅✅✅✅✅

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,50 @@
 import React from 'react';
-
+import './Footer.scss'
 const Footer = () => {
   return(
-      <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste cum, ab et quos alias natus vel nobis tempora harum dignissimos reiciendis corrupti magni repudiandae expedita eligendi fugit, facilis, asperiores doloremque tempore similique deserunt odio ut placeat. Saepe assumenda nihil amet dolor quia omnis beatae quo deleniti quisquam quis? Libero, nihil?</h1>
+    <>
+      <footer>
+        <div className='one'>
+          <h2>Education</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis explicabo, perferendis ad officia distinctio similique laudantium error at aliquid in?</p>
+          <h2>Follow us</h2>
+          <div className="social">
+            <i>hi</i>
+            <i>hi</i>
+            <i>hi</i>
+            <i>hi</i>
+          </div>
+        </div>
+        <div className="two">
+          <h2>Feature links</h2>
+          <ul className="link">
+            <li><span>❭</span> <a href="#">About us</a></li>
+            <li><span>❭</span><a href="#">Graduation</a></li>
+            <li><span>❭</span><a href="#">Scholarship</a></li>
+            <li><span>❭</span><a href="#">Admissions</a></li>
+            <li><span>❭</span><a href="#">International</a></li>
+            <li><span>❭</span><a href="#">Events</a></li>
+          </ul>
+        </div>
+        <div className="two">
+          <h2>Support</h2>
+          <ul className="link">
+            <li><span>❭</span><a href="#">Language</a></li>
+            <li><span>❭</span><a href="#">WordPress</a></li>
+            <li><span>❭</span><a href="#">Privacy</a></li>
+            <li><span>❭</span><a href="#">FAQs</a></li>
+            <li><span>❭</span><a href="#">Support</a></li>
+          </ul>
+        </div>
+        <div className="four">
+          <h2>Contact us</h2>
+          <p className='address'>59-home 39, Dom Chilonzor Tashkent Uzbekistan</p>
+          <p><span>📞</span> +017 4845 7269</p>
+          <p><span>💬</span>asadullohruziev@gmail.com</p>
+          <p><span>🌐</span>asadullohruziev.com</p>
+        </div>
+      </footer>
+    </>
   );
 };
 
